13c6e06a210c7fbf211f86519effb9b0f3125929
Pedro Cunha <pvc3@cin.ufpe.br>
2023-02-26 00:28:55 -0300

    adding injector in controladores

--- RUP/backend/negocio/controladores/controladorCadastro.py
+++ RUP/backend/negocio/controladores/controladorCadastro.py
@@ -1,11 +1,12 @@
 from negocio.entidades.cliente import Cliente
 from negocio.registros.registroClientes import RegistroClientes
-from utils.utils import myContainer
+from utils.utils import Injector
 
 class ControladorCadastro:
 
     def __init__(self) -> None:
-        self.registroClientes = RegistroClientes(myContainer.repositorioClientes)
+        injector = Injector()
+        self.registroClientes = RegistroClientes(injector.repositorioClientes)
     
     def cadastroCliente(self, email, senha, cpf)-> Cliente:
         clienteNovo = Cliente(email, senha, cpf)
--- RUP/backend/negocio/controladores/controladorLogin.py
+++ RUP/backend/negocio/controladores/controladorLogin.py
@@ -1,11 +1,12 @@
 from negocio.entidades.cliente import Cliente
 from negocio.registros.registroClientes import RegistroClientes
-from utils.utils import myContainer
+from utils.utils import Injector
 
 class ControladorLogin:
 
     def __init__(self) -> None:
-        self.registroClientes = RegistroClientes(myContainer.repositorioClientes)
+        injector = Injector()
+        self.registroClientes = RegistroClientes(injector.repositorioClientes)
 
-    def loginCliente(self,email,senha) -> Cliente:
+    def loginCliente(self,email, senha) -> Cliente:
         return self.registroClientes.fazLogin(email, senha)
