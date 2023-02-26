a0cbb6fb2b3e45f7df9c4229c964d94226c942c4
Pedro Cunha <pvc3@cin.ufpe.br>
2023-02-26 00:30:08 -0300

    change to injector

--- RUP/backend/negocio/fachada.py
+++ RUP/backend/negocio/fachada.py
@@ -1,4 +1,3 @@
-from typing import Tuple
 from flask import jsonify, Response
 from utils.utils import SingletonMeta
 from negocio.entidades.cliente import Cliente
@@ -6,7 +5,7 @@ from negocio.controladores.controladorLogin import ControladorLogin
 from negocio.controladores.controladorCadastro import ControladorCadastro
 from interface_negocio_dados.iFabricaRepositorios import IFabricaAbstrataRepositorios
 from negocio.fabricaRepositoriosMemoria import FabricaRepositoriosMemoria
-from utils.utils import myContainer
+from utils.utils import Injector
 
 class Fachada(metaclass=SingletonMeta):
     
@@ -16,27 +15,28 @@ class Fachada(metaclass=SingletonMeta):
         else:
             pass
 
+
         repositorioClientes = self.__fabricaRepositorio.criaRepositorioClientes()
         repositorioEstoque = self.__fabricaRepositorio.criaRepositorioEstoque()
         repositorioPedidos = self.__fabricaRepositorio.criaRepositorioPedido()
-        self.repo = repositorioClientes
-        myContainer.repositorioClientes.override(repositorioClientes)
-        myContainer.repositorioEstoque.override(repositorioEstoque)
-        myContainer.repositorioPedidos.override(repositorioPedidos)
+        
+        myContainer = Injector()
+
+        myContainer.repositorioClientes = repositorioClientes
+        myContainer.repositorioEstoque = repositorioEstoque
+        myContainer.repositorioPedidos = repositorioPedidos
         
         self.__controladorLogin = ControladorLogin()
         self.__controladorCadastro = ControladorCadastro()
-        
-    def pegaalgo(self):
-        return jsonify(self.repo.getAll())
-    
+
     def fazLogin(self, email: str, senha: str) -> Response:
         try:
             cliente = self.__controladorLogin.loginCliente(email, senha)
             response = jsonify(cliente.__dict__)
             response.status_code = 200
             return response
-        except:
+        except Exception as err:
+            print(err)
             return Response(status=204)
 
     def fazCadastro(self, email: str, senha: str, cpf: str) -> Response:
