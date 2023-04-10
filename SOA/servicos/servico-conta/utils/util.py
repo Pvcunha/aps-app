84d61ec53f6499285f80638a58f3a8d385c372fa
Pedro Cunha <pvc3@cin.ufpe.br>
2023-04-09 22:06:44 -0300

    fix: conserta implementacao do singleton

--- SOA/servicos/servico-conta/utils/util.py
+++ SOA/servicos/servico-conta/utils/util.py
@@ -4,6 +4,6 @@ class Singleton(type):
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instance:
-            cls._instance[cls] = super(Singleton, cls).__call__(cls, *args, **kwargs)
-            return cls._instance[cls]
+            cls._instance[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            
         return cls._instance[cls]
