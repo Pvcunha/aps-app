fbf10b78ce96455e6a6c4a98697570435f76c03e
Pedro Cunha <pvc3@cin.ufpe.br>
2023-04-10 20:41:04 -0300

    feat: adicionando consul para registro de servicos

--- SOA/servicos/servico-pedido/app.py
+++ SOA/servicos/servico-pedido/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from controllers.controllerPedido import ControllerPedido
 from middlewares.auth import MiddlewareAuth
+import consul
 
 def criaApp():
     app = Flask(__name__)
     
     controllerPedido = ControllerPedido()
+
 
     @app.route('/')
     def home():
@@ -34,4 +36,17 @@ def criaApp():
     return app
 
 if __name__ == '__main__':
+    client = consul.Consul(host='discovery', port=8500)
+    
+    service_name = 'servico-pedido'
+    service_port = 3001
+
+    client.agent.service.register(
+        
+        name=service_name,
+        service_id=service_name,
+        port=service_port,
+        check=consul.Check.http(f'http://servico-pedido:{service_port}', interval='10s')    
+    )
+
     criaApp().run(debug=True, host="0.0.0.0", port=3001)
