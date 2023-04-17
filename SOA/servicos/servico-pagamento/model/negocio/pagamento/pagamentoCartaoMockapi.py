74e95e7bdbea7d3f9c92b75de66d6994686c6fc5
Pedro Cunha <pvc3@cin.ufpe.br>
2023-04-17 16:55:34 -0300

    fronteira mock api

--- SOA/servicos/servico-pagamento/model/negocio/pagamento/pagamentoCartaoMockapi.py
+++ SOA/servicos/servico-pagamento/model/negocio/pagamento/pagamentoCartaoMockapi.py
@@ -2,7 +2,8 @@ import requests
 import json
 from typing import Dict, Any
 from ..pedido.pedido import Pedido
+from middlewares.fronteiraMockAPI import PagamentoCartaoMockapi
 
 class PagamentoCartaoMockapi:
     def pagar(self, **args) -> Dict[Pedido, Any]:
-        return self.pagar(args)
+        return PagamentoCartaoMockapi.pagar(args)
